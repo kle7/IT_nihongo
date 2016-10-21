4ebfc2fc744445b4cec88cc052728f8d4c9d8432
leduykhanhhedspi <leduykhanh.hedspi@gmail.com>
2016-10-21 13:31:43 +0700

    CRUD movie

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
-  before_action :check_admin
   def home
   end
 
@@ -7,10 +6,4 @@ class StaticPagesController < ApplicationController
   end
 
   private 
-  def check_admin
-    if current_user.admin?
-      flash[:danger] = t :not_have_permission
-      redirect_to admin_root_path   
-    end 
-  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,12 @@ class Movie < ActiveRecord::Base
   has_many :movie_genres, dependent: :destroy
   has_many :actors, through: :movie_actors
   has_many :movie_actors, dependent: :destroy
+
+  validates :name, presence: true
+  validates :content, presence: true
+
+  accepts_nested_attributes_for :genres
+
+  include Bootsy::Container
+  mount_uploader :image, MovieImageUploader
 end
