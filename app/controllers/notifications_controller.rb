7584364bfd0f3587a63742aacfa2784d799b245d
TrinhKien94 <kieict.94@gmail.com>
2016-12-09 13:54:13 +0700

    create follow request list and accept follow notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,6 +9,10 @@ class NotificationsController < ApplicationController
     end
   end
 
+  def follow_list
+    @notifications = current_user.recived_notifications.where.not(reciver_status: 2)
+  end
+
   def show_list
     notifications = current_user.recived_notifications.where.not(reciver_status: 2)
     senders = []
@@ -32,10 +36,26 @@ class NotificationsController < ApplicationController
     notification = Notification.find(params[:notification_id])
     notification.reciver_status=2;
     notification.save
+    n = Notification.new
+    n.sender_id = current_user.id
+    n.reciver_id = notification.sender_id
+    n.reciver_status = 0 #hasn't been read
+    n.sender_status = 1 #sended
+    n.content = "respone_follow"
+    n.link = "accepted"
+    n.save
   end
 
   def decline
     notification = Notification.find(params[:notification_id])
+    n = Notification.new
+    n.sender_id = current_user.id
+    n.reciver_id = notification.sender_id
+    n.reciver_status = 0 #hasn't been read
+    n.sender_status = 1 #sended
+    n.content = "respone_follow"
+    n.link = "declined"
+    n.save
     notification.destroy
   end
 end
